36529e84c197b8061e3cd9160ca42df6f33337c7
neifialcantara <neifialcantara@gmail.com>
2023-06-13 17:55:37 +0200

    fix QA issues

--- internal/platform/rest/handler/post_receive_webhook.go
+++ internal/platform/rest/handler/post_receive_webhook.go
@@ -11,12 +11,12 @@ import (
 
 func PostReceiveWebhook(certification update.CertificationAnchor, secretKey string, enforceTolerance bool) gin.HandlerFunc {
 	return func(ctx *gin.Context) {
+
 		var webhookRequest WebhookRequest
 		if err := ctx.BindJSON(&webhookRequest); err != nil {
 			ctx.JSON(http.StatusBadRequest, "invalid json body")
 			return
 		}
-
 		bloockSignature := ctx.GetHeader("Bloock-Signature")
 
 		webhookClient := client.NewWebhookClient()
@@ -37,7 +37,7 @@ func PostReceiveWebhook(certification update.CertificationAnchor, secretKey stri
 		}
 
 		if err := certification.UpdateAnchor(ctx, request.UpdateCertificationAnchorRequest{
-			AnchorId: webhookRequest.Network.AnchorId,
+			AnchorId: webhookRequest.Data.Network.AnchorId,
 			Payload:  webhookRequest,
 		}); err != nil {
 			ctx.JSON(http.StatusInternalServerError, err.Error())
@@ -49,18 +49,24 @@ func PostReceiveWebhook(certification update.CertificationAnchor, secretKey stri
 }
 
 type WebhookRequest struct {
-	CreatedAt    int  `json:"created_at"`
-	Finalized    bool `json:"finalized"`
-	Id           int  `json:"id"`
-	MessageCount int  `json:"message_count"`
-	Network      struct {
-		AnchorId  int    `json:"anchor_id"`
-		CreatedAt int    `json:"created_at"`
-		Name      string `json:"name"`
-		Status    string `json:"status"`
-		Test      bool   `json:"test"`
-		TxHash    string `json:"tx_hash"`
-	} `json:"network"`
-	Root string `json:"root"`
-	Test bool   `json:"test"`
+	WebhookId string `json:"webhook_id"`
+	RequestId string `json:"request_id"`
+	Type      string `json:"type"`
+	CreatedAt int    `json:"created_at"`
+	Data      struct {
+		CreatedAt    int  `json:"created_at"`
+		Finalized    bool `json:"finalized"`
+		Id           int  `json:"id"`
+		MessageCount int  `json:"message_count"`
+		Network      struct {
+			AnchorId  int    `json:"anchor_id"`
+			CreatedAt int    `json:"created_at"`
+			Name      string `json:"name"`
+			Status    string `json:"status"`
+			Test      bool   `json:"test"`
+			TxHash    string `json:"tx_hash"`
+		} `json:"network"`
+		Root string `json:"root"`
+		Test bool   `json:"test"`
+	} `json:"data"`
 }
